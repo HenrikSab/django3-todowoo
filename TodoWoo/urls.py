58943b067f4f63caf62845ac6050658e69bf15d6
sabroe <hs@sabroe.dk>
2020-09-30 17:00:59 +0200

    my 2 commit

--- TodoWoo/urls.py
+++ TodoWoo/urls.py
@@ -22,15 +22,16 @@ urlpatterns = [
 
 
 #auth
-
+    path('create/', views.createtodo, name='createtodo'),
     path('signup/', views.signupuser, name='signupuser'),
     path('logout/', views.logoutuser, name='logoutuser'),
     path('login/', views.loginuser, name='loginuser'),
     path('', views.home, name='home'),
-
-
-
-#signup
     path('current/', views.currenttodos, name='currenttodos'),
+    path('todo/<int:todo_pk>', views.viewtodo, name='viewtodo'),
+    path('todo/<int:todo_pk>/complete', views.completetodo, name='completetodo'),
+    path('todo/<int:todo_pk>/delete', views.deletetodo, name='deletetodo'),
+    path('done/', views.donetodos, name='donetodos'),
+    path('copy/<int:todo_pk>/', views.copytodo, name='copytodo'),
 
 ]
