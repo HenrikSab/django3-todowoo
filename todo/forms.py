995b543048d4cde256aa34a769ca58bb9e326c60
sabroe <hs@sabroe.dk>
2020-10-02 17:30:45 +0200

    my 3 commit

--- todo/forms.py
+++ todo/forms.py
@@ -5,4 +5,4 @@ from .models import Todo
 class TodoForm(ModelForm):
     class Meta:
         model = Todo
-        fields =['title', 'memo', 'important',]
+        fields =['title', 'memo', 'history', 'important',]
